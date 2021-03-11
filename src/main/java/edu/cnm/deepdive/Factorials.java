c25dee7011a54bd55083685dccd6f899f6ca76f6
rbrazell1 <brazellrussell@gmail.com>
2021-03-10 20:56:23 -0700

    Added method for string palindromes

--- src/main/java/edu/cnm/deepdive/Factorials.java
+++ src/main/java/edu/cnm/deepdive/Factorials.java
@@ -14,4 +14,21 @@ public class Factorials {
     return limit * computeRecursive(limit - 1);
   }
 
-}
+  public static boolean checkRecursive(String palindrome) {
+    char[] decomp = palindrome.toCharArray();
+
+    for (int i = 0; i < decomp.length; i++) {
+      char x = decomp[i];
+      char y = decomp[decomp.length -1 - i];
+      Character.compare(x, y);
+      if (x != y) {
+        return false;
+      }
+    }
+
+//    for (int k = 0; k < decomp.length; k++) {
+//
+//    }
+    return true;
+    }
+  }
