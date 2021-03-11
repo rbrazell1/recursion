930eb6cfe4f81cddfda0c6e1295945e9e1916d9c
rbrazell1 <brazellrussell@gmail.com>
2021-03-10 21:14:16 -0700

    Added test method for string palindromes

---
+++ src/test/java/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+class PalindromesTest {
+
+  @ParameterizedTest(name = "[{index}] Asserting checkRecursive ({0}) == {1}")
+  @CsvFileSource(resources = "palindromes-test-case.csv" , numLinesToSkip = 1)
+  void checkRecursive(String input, boolean output) {
+    assertEquals(output, Factorials.checkRecursive(input));
+  }
+}
